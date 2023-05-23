bf6e0875b803bc6e1d90eb588e17656f3251c23d
Normandie3 <stannardmak@yandex.ru>
2023-05-23 16:40:51 +0300

    fix review

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,9 @@
 let popupElement = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__form');
 let popupOpenButtonElement = document.querySelector('.profile__edit-button');
 let popupCloseButtonElement = popupElement.querySelector('.popup__close');
-
 let nameInput = popupElement.querySelector('.popup__input_type_name');
 let jobInput = popupElement.querySelector('.popup__input_type_job');
-
 let nameProfile = document.querySelector('.profile__name');
 let jobProfile = document.querySelector('.profile__job');
 
@@ -16,12 +15,14 @@ function openPopup () {
 function closePopup () {
     popupElement.classList.remove('popup_opened');
 }
-popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
-function handleFormSubmit (evt) {
+
+function handlerFormSubmit (evt) {
     evt.preventDefault();
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
     closePopup();
 }
-popupElement.addEventListener('submit', handleFormSubmit);
+
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handlerFormSubmit);
